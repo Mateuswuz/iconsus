0fc1b4be0fd4fa60eea88834f529a47bc437ddae
Mateuswuz <116360372+Mateuswuz@users.noreply.github.com>
2022-12-09 16:02:06 -0300

    Add files via upload

---
+++ assets/preload/stages/ico.lua
@@ -0,0 +1,14 @@
+function onCreate()
+
+
+	makeLuaSprite('Base', 'iconbg',-800,-60)
+	addLuaSprite('Base',false)
+	setLuaSpriteScrollFactor('Base', 1, 1)
+        scaleObject('Base', 1, 1);
+
+
+
+
+
+
+end
